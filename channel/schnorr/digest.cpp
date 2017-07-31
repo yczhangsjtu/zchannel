d18a74a780234d6964944d29aa02c9e596748665
Yuncong Zhang <shjdzhangyuncong@sjtu.edu.cn>
2017-07-31 18:35:11 +0800

    Temp store

--- channel/schnorr/digest.cpp
+++ channel/schnorr/digest.cpp
@@ -1,3 +1,4 @@
+#include <vector>
 #include <openssl/sha.h>
 #include "digest.h"
 
@@ -10,5 +11,14 @@ SHA256Digest::SHA256Digest(const unsigned char* msg1, size_t len1, const unsigne
 	SHA256_Init(&ctx);
 	SHA256_Update(&ctx,msg1,len1);
 	SHA256_Update(&ctx,msg2,len2);
+	SHA256_Final(md.data(),&ctx);
+}
+
+SHA256Digest::SHA256Digest(std::vector<std::array<unsigned char,size()>> mds) {
+	SHA256_CTX ctx;
+	SHA256_Init(&ctx);
+	for(size_t i = 0; i < mds.size(); i++) {
+		SHA256_Update(&ctx,mds.data(),size());
+	}
 	SHA256_Final(md.data(),&ctx);
 }
--- channel/schnorr/digest.h
+++ channel/schnorr/digest.h
@@ -3,6 +3,7 @@
 
 #include <array>
 #include <string>
+#include <vector>
 
 template<size_t n>
 class Digest {
@@ -11,7 +12,7 @@ protected:
 public:
 	Digest(){}
 	Digest(std::array<unsigned char,n> md):md(md){}
-	size_t size()const{return n;}
+	static constexpr size_t size(){return n;}
 	const unsigned char* data()const{return md.data();}
 	const std::array<unsigned char,n> getArray() const {return md;}
 };
@@ -22,6 +23,7 @@ public:
 	SHA256Digest(const unsigned char* msg, size_t len);
 	SHA256Digest(const unsigned char* msg1, size_t len1, const unsigned char* msg2, size_t len2);
 	SHA256Digest(std::string msg):SHA256Digest((const unsigned char *)msg.c_str(),msg.size()){}
+	SHA256Digest(std::vector<std::array<unsigned char,size()>> mds);
 };
 
 #endif
